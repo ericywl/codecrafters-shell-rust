d8b1fcb617870c8d2e745ebd05366089c13dd7f6
Eric Yap <ericywl@live.com>
2025-03-12 21:12:56 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,46 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+use anyhow::Context;
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+fn main() -> anyhow::Result<()> {
+    repl()
+}
 
-    let mut iter = input.split_whitespace();
-    let command = iter.next();
-    match command {
-        Some(command) => command_not_found(&command),
-        None => !todo!(),
+fn repl() -> anyhow::Result<()> {
+    loop {
+        let mut stdout = io::stdout().lock();
+        stdout
+            .write_all("$ ".as_bytes())
+            .context("failed to write prompt")?;
+        stdout.flush().context("failed to flush prompt")?;
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin
+            .read_line(&mut input)
+            .context("failed to read input")?;
+
+        // Parse command
+        let mut iter = input.split_whitespace();
+        let command = iter.next();
+
+        // Execute command
+        let mut output = match command {
+            Some(command) => command_not_found(&command),
+            None => continue,
+        };
+
+        // Write output
+        output.push(b'\n');
+        stdout
+            .write_all(&output)
+            .context("failed to write output")?;
+        stdout.flush().context("failed to flush output")?;
     }
 }
 
-fn command_not_found(command: &str) {
-    let mut stdout = io::stdout();
-    stdout
-        .write_all(format!("{command}: command not found").as_bytes())
-        .unwrap();
+fn command_not_found(command: &str) -> Vec<u8> {
+    format!("{command}: command not found").into()
 }
