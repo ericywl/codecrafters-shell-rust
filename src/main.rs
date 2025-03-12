d68edd4adc87e72cab5a3d9fb4691faa50af2b49
Eric Yap <ericywl@live.com>
2025-03-12 20:56:14 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,18 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    let mut iter = input.split_whitespace();
+    let command = iter.next();
+    match command {
+        Some(command) => command_not_found(&command),
+        None => !todo!(),
+    }
+}
+
+fn command_not_found(command: &str) {
+    let mut stdout = io::stdout();
+    stdout
+        .write_all(format!("{command}: command not found").as_bytes())
+        .unwrap();
 }
