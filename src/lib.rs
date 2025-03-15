d3c0aac5c3b24489af194ae6ce60e5173f770842
Eric Yap <ericywl@live.com>
2025-03-15 21:21:15 +0800

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -53,98 +53,108 @@ pub fn repl() -> anyhow::Result<()> {
 fn parse_args(args_str: &str) -> Result<Vec<String>, String> {
     let args_str = args_str.trim();
     let mut args: Vec<String> = Vec::new();
+    let mut next_arg = String::new();
     let mut start_idx = 0;
     let mut in_single_quotes = false;
     let mut prev_single_end_quote_idx: Option<usize> = None;
     let mut in_double_quotes = false;
     let mut prev_double_end_quote_idx: Option<usize> = None;
-    for (idx, c) in args_str.char_indices() {
-        match c {
-            '"' => {
-                // Treat double quotes as per normal if we are in single quotes
-                if in_single_quotes {
-                    continue;
-                }
+    let mut escaped = false;
 
+    for (idx, ch) in args_str.char_indices() {
+        match ch {
+            // If backslash is escaped or in single quotes, treat it as per normal char
+            '\\' if !escaped && !in_single_quotes && !in_double_quotes => escaped = true,
+            // If double quote is in single quotes or escaped, treat it as per normal
+            '"' if !in_single_quotes && !escaped => {
                 in_double_quotes = !in_double_quotes;
                 if in_double_quotes {
+                    // Ignore the starting double quote
                     start_idx = idx;
                     continue;
                 }
-
-                let arg = &args_str[start_idx + 1..idx];
-                if !arg.is_empty() {
+                if !next_arg.is_empty() {
                     match prev_double_end_quote_idx {
                         // Combine two double quoted strings e.g.
                         // `"hello""world"` => `helloworld`
                         Some(pdeq_idx) => {
                             if pdeq_idx == start_idx - 1 {
-                                let len = args.len();
-                                args[len - 1].push_str(&arg);
+                                merge_next_arg(&mut args, &mut next_arg);
                             } else {
-                                args.push(arg.to_owned())
+                                push_next_arg(&mut args, &mut next_arg)
                             }
                         }
-                        None => args.push(arg.to_owned()),
+                        None => push_next_arg(&mut args, &mut next_arg),
                     };
                     prev_double_end_quote_idx = Some(idx);
                 }
                 start_idx = idx + 1;
             }
-            '\'' => {
-                // Treat single quotes as per normal if we are in double quotes
-                if in_double_quotes {
-                    continue;
-                }
-
+            // If single quote is in double quotes or escaped, treat it as per normal
+            '\'' if !in_double_quotes && !escaped => {
                 in_single_quotes = !in_single_quotes;
                 if in_single_quotes {
+                    // Ignore the ending double quote
                     start_idx = idx;
                     continue;
                 }
-
-                let arg = &args_str[start_idx + 1..idx];
-                if !arg.is_empty() {
+                if !next_arg.is_empty() {
                     match prev_single_end_quote_idx {
                         // Combine two single quoted strings e.g.
                         // `'hello''world'` => `helloworld`
                         Some(pseq_idx) => {
                             if pseq_idx == start_idx - 1 {
-                                let len = args.len();
-                                args[len - 1].push_str(&arg);
+                                merge_next_arg(&mut args, &mut next_arg);
                             } else {
-                                args.push(arg.to_owned())
+                                push_next_arg(&mut args, &mut next_arg);
                             }
                         }
-                        None => args.push(arg.to_owned()),
+                        None => push_next_arg(&mut args, &mut next_arg),
                     };
                     prev_single_end_quote_idx = Some(idx);
                 }
                 start_idx = idx + 1;
             }
-            _ if c.is_whitespace() => {
-                if !in_single_quotes && !in_double_quotes {
-                    let arg = &args_str[start_idx..idx];
-                    if !arg.is_empty() {
-                        args.push(arg.to_owned());
-                    }
-                    start_idx = idx + 1;
+            // If char is not whitespace or is escaped, treat is as per normal
+            _ if (ch.is_whitespace() && !escaped) => {
+                if in_single_quotes || in_double_quotes {
+                    next_arg.push(ch);
+                    continue;
                 }
+                if !next_arg.is_empty() {
+                    push_next_arg(&mut args, &mut next_arg);
+                }
+                start_idx = idx + 1;
             }
-            _ => (),
+            _ => {
+                if escaped {
+                    escaped = false
+                }
+                next_arg.push(ch);
+            }
         }
     }
 
-    if in_single_quotes {
+    if in_single_quotes || in_double_quotes {
         return Err("quotes unfinished".into());
     }
 
-    let arg = &args_str[start_idx..];
-    if !arg.trim().is_empty() {
-        args.push(arg.to_owned());
+    if !next_arg.trim().is_empty() {
+        push_next_arg(&mut args, &mut next_arg);
     }
 
     Ok(args)
+}
+
+fn push_next_arg(args: &mut Vec<String>, next_arg: &mut String) {
+    args.push(next_arg.clone());
+    *next_arg = String::new()
+}
+
+fn merge_next_arg(args: &mut Vec<String>, next_arg: &mut String) {
+    let len = args.len();
+    args[len - 1].push_str(next_arg);
+    *next_arg = String::new()
 }
 
 #[cfg(test)]
@@ -205,5 +215,37 @@ mod test {
         assert!(args.is_ok());
         let args = args.unwrap();
         assert_eq!(args, vec!["'quz''hello'"]);
+    }
+
+    #[test]
+    fn test_parse_args_backslash() {
+        let args = parse_args("world\\ \\ \\ \\\\\\ \\ \\ script");
+        assert!(args.is_ok());
+        let args = args.unwrap();
+        assert_eq!(args, vec!["world   \\   script"]);
+    }
+
+    #[test]
+    fn test_parse_args_backslash_in_single_quoted() {
+        let args = parse_args("'world\\ \\script'");
+        assert!(args.is_ok());
+        let args = args.unwrap();
+        assert_eq!(args, vec!["world\\ \\script"]);
+    }
+
+    #[test]
+    fn test_parse_args_backslash_in_double_quoted() {
+        let args = parse_args("\"before\\   after\"");
+        assert!(args.is_ok());
+        let args = args.unwrap();
+        assert_eq!(args, vec!["before\\   after"]);
+    }
+
+    #[test]
+    fn test_parse_args_backslash_before_quotes() {
+        let args = parse_args("\\'\\\"hello script\\\"\\'");
+        assert!(args.is_ok());
+        let args = args.unwrap();
+        assert_eq!(args, vec!["'\"hello", "script\"'"]);
     }
 }
